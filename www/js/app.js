4057000aa7441621d95c582fc78b10a0e1ac9b49
youngmin <youngmin.k@gmail.com>
2014-12-17 17:09:20 +0900

    profile image updated

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('doresolApp', ['ngMaterial','ui.router', 'firebase', 'env'])
+angular.module('doresolApp', ['ngMaterial','ui.router', 'firebase', 'env', 'ngAnimate'])
 
 .run(function($location,  $rootScope, $state, Auth, User ,Composite) {
 
